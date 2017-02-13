f78fe6b3c2b28a8514d5973bbd16166f8f8a7ee4
krzysztof.graczyk <allez@wp.pl>
2017-02-13 15:49:24 +0100

    Basic configuration

---
+++ src/main/java/tempstore/rest/Read.java
@@ -0,0 +1,13 @@
+package tempstore.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import tempstore.dto.TempRead;
+
+@RestController
+public class Read {
+
+	public void save(TempRead reading) {
+
+	}
+}
---
+++ src/main/java/tempstore/spring/configuration/SpringConfiguration.java
@@ -0,0 +1,10 @@
+package tempstore.spring.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("tempstore")
+public class SpringConfiguration {
+
+}
