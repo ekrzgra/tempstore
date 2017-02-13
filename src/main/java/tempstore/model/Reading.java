0c4ac48dca98f033f9be747466818ea2079850d7
krzysztof.graczyk <allez@wp.pl>
2017-02-13 10:24:20 +0100

    First simple persistence configuration.

---
+++ src/main/java/tempstore/model/Reading.java
@@ -0,0 +1,39 @@
+package tempstore.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Reading {
+
+	@Id
+	private String id;
+	private Date readingDate;
+	private int temperatureValue;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getReadingDate() {
+		return readingDate;
+	}
+
+	public void setReadingDate(Date readingDate) {
+		this.readingDate = readingDate;
+	}
+
+	public int getTemperatureValue() {
+		return temperatureValue;
+	}
+
+	public void setTemperatureValue(int temperatureValue) {
+		this.temperatureValue = temperatureValue;
+	}
+}
