e7d8361a909890a7ff85ae65792954ebf17767c3
krzysztof.graczyk <allez@wp.pl>
2017-02-13 22:36:50 +0100

    First controller and rest spring configuration added
    Change-Id: I3bba8e9adb3e98b9d035d097455f28fbb975fc01

--- src/main/java/tempstore/rest/Read.java
+++ src/main/java/tempstore/rest/Read.java
@@ -1,5 +1,7 @@
 package tempstore.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import tempstore.dto.TempRead;
@@ -7,6 +9,7 @@ import tempstore.dto.TempRead;
 @RestController
 public class Read {
 
+	@RequestMapping(value = "/read", method = RequestMethod.GET)
 	public void save(TempRead reading) {
 
 	}
