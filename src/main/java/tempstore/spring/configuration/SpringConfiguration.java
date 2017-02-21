de78b09a79d63e5672a26b160459c48fd3e4eb73
krzysztof.graczyk <allez@wp.pl>
2017-02-21 22:58:06 +0100

    Define simple data source

--- src/main/java/tempstore/spring/configuration/SpringConfiguration.java
+++ src/main/java/tempstore/spring/configuration/SpringConfiguration.java
@@ -1,7 +1,18 @@
 package tempstore.spring.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jndi.JndiObjectFactoryBean;
 
 @Configuration
 public class SpringConfiguration {
+
+	@Bean
+	public JndiObjectFactoryBean dataSource() {
+		JndiObjectFactoryBean jndiObjectFB = new JndiObjectFactoryBean();
+		jndiObjectFB.setJndiName("jdbc/tempstore");
+		jndiObjectFB.setResourceRef(true);
+		jndiObjectFB.setProxyInterface(javax.sql.DataSource.class);
+		return jndiObjectFB;
+	}
 }
